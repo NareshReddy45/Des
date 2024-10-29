4f140347924e8933e3279b5c4d0032357de8dc30
NareshReddy45 <naresh.nalideva45@gmail.com>
2024-10-29 09:24:04 +0000

    push the code

--- src/components/DestinationItem/index.js
+++ src/components/DestinationItem/index.js
@@ -1,1 +1,14 @@
-// Write your code here
+import './index.css'
+
+const DestinationItem = ({filteredDestinations}) => (
+  <ul className="destination-list">
+    {filteredDestinations.map(item => (
+      <li key={item.id} className="destination-item">
+        <img src={item.imgUrl} alt={item.name} className="image" />
+        <p className="heading">{item.name}</p>
+      </li>
+    ))}
+  </ul>
+)
+
+export default DestinationItem
--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -1,1 +1,43 @@
-// Write your code here
+import {useState} from 'react'
+import './index.css'
+import DestinationItem from '../DestinationItem'
+
+const DestinationSearch = ({destinationsList}) => {
+  const [inputValue, setInputValue] = useState('')
+
+  const handleInputChange = event => {
+    setInputValue(event.target.value)
+  }
+
+  // Filter destinations based on inputValue, case-insensitive
+  const filteredDestinations = destinationsList.filter(destination =>
+    destination.name.toLowerCase().includes(inputValue.toLowerCase()),
+  )
+
+  return (
+    <div className="bg-container">
+      <h1>Destination Search</h1>
+
+      {/* Search input container */}
+      <div className="input-container">
+        <input
+          type="search"
+          placeholder="Search..."
+          onChange={handleInputChange}
+          value={inputValue}
+          className="input-with-icon"
+        />
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
+          alt="search icon"
+          className="search-icon"
+        />
+      </div>
+
+      {/* Pass filteredDestinations to DestinationItem */}
+      <DestinationItem filteredDestinations={filteredDestinations} />
+    </div>
+  )
+}
+
+export default DestinationSearch
